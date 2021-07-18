d29b49b36bfc95a2344663bc65656115fa74fb50
r4ksh1t0011 <rakshitawasthi17@gmail.com>
2021-07-18 23:01:51 +0530

    New dashboard update

--- my-app/src/Components/MainScreen/Dashboard.js
+++ my-app/src/Components/MainScreen/Dashboard.js
@@ -24,9 +24,66 @@ function Dashboard(){
     const [dataNotFound,setDataNotFound] = useState("Loading...");
     const [startTime,setStartTime] = useState(1000);
     const [startNow,setStartNow] = useState(false);
-    function activateTrigger(){
-        setStartNow(true);
-    }
+
+        useEffect(()=>{
+            axios({
+                method:"GET",
+                url:"http://127.0.0.1:4000/mainScr/teacher/courses"
+            }).then((results)=>{
+                if(results.data!="No"){
+                setDat(results.data);
+
+                setCourseData(true);
+            }
+            else if(results.data==="No"){
+                axios({
+                    method:"GET",
+                    url:"http://127.0.0.1:4000/mainScr/teacher/quizDat"
+                }).then((respon)=>{
+                    let datq = respon.data;
+                    let len1=  datq.length;
+                    let datArray = [];
+                    console.log(respon.data);
+                        let datq1 = datq.map(dat=>JSON.parse(dat));
+                        const newArr = [...datq1];
+                        setStudQuiz(newArr);
+                        console.log(newArr);
+                        let currentDate = new Date();
+                        let quizTimings = [];
+                        currentDate = currentDate.getTime();
+                        for(let j1=0;j1<len1;j1++){
+                          let quizDate = new Date(newArr[j1].date);
+                          let quizActTime = newArr[j1].time;
+                          let quizActT1 = quizActTime.split(0,2);
+                          let quizActT2 = quizActTime.split(3,5);
+                          quizActT1 = 3600*1000*parseInt(quizActT1);
+                          quizActT2=60*1000*parseInt(quizActT2);
+
+                          quizDate = quizDate.getTime();
+                          quizDate+=(quizActT1+quizActT2);
+                          let remTime = quizDate-currentDate;
+                          console.log(remTime);
+
+                          let dataStore = {time:remTime,course:newArr[j1].course}
+                          quizTimings.push(dataStore);
+                        }
+                        console.log(quizTimings);
+                        quizTimings.forEach((item) => {
+                          if(item.time>=0){
+                            setTimeout(activateTrigger,item.time);
+                            }
+                        });
+
+                }).catch(error=>console.log(error));
+
+            }
+            }).catch(err=>console.log(err));
+
+            return(
+              <h1 style={{color:"red"}}>Connection error</h1>
+            )
+        },[]);
+
     useEffect(()=>{
         axios({
             method:"GET",
@@ -36,43 +93,14 @@ function Dashboard(){
         })
         .catch(err=>console.log(err));
     },[])
-    
-    useEffect(()=>{
-        axios({
-            method:"GET",
-            url:"http://127.0.0.1:4000/mainScr/teacher/courses"
-        }).then((results)=>{
-            if(results.data!="No"){
-            setDat(results.data);
-        
-            setCourseData(true);
+    function activateTrigger(){
+        if(startNow===false){
+        setStartNow(true);
         }
-        else if(results.data==="No"){
-            axios({
-                method:"GET",
-                url:"http://127.0.0.1:4000/mainScr/teacher/quizDat"
-            }).then((respon)=>{
-                let datq = respon.data;
-                let len1=  datq.length;
-                let datArray = [];
-                console.log(respon.data);
-                    let datq1 = datq.map(dat=>JSON.parse(dat));
-                    const newArr = [...datq1];
-                    setStudQuiz(newArr);
-                    console.log(newArr);
-                    setTimeout(()=>{
-                        activateTrigger();
-                    },12000)
-            }).catch(error=>console.log(error));
-
-        }
-        }).catch(err=>console.log(err));
-
-       
-    },[]);
+    }
     function RenderQuizData(props){
         return <div className="renderQuizData" key={x1++}>
-        
+
         <li>{props.date}</li>
         <li>{props.time}</li>
         <li>{props.course}</li>
@@ -96,28 +124,28 @@ function Dashboard(){
     else if(position==="student"){
         let startTime = new Date();
         startTime = startTime.getTime();
-        
+
         if(studQuiz.length===0){
             setTimeout(()=>{
                 let curTime = new Date();
                 curTime = curTime.getTime();
                 setDataNotFound("No test scheduled right now");
-            },10000);
+            },30000);
             return <h1 style={{color:"white"}}>{dataNotFound}</h1>
         }
-        
+
         console.log(studQuiz);
         let myarr = [...studQuiz];
         console.log(myarr[0].date);
         return <div>
         <Head></Head>
         {myarr.map(RenderQuizData)}
-        
+
         </div>
     }
     else{
         return <h1 style={{color:"white"}}>Loading...</h1>
     }
-    
+
 }
 export default Dashboard;
